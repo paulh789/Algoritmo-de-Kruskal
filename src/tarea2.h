3776f83e1154da3662863eaadd93f57f2f6b239f
unknown <paul.holmer789@gmail.com>
2025-06-06 15:39:33 -0400

    feat: create tarea2.h file

---
+++ src/tarea2.h
@@ -0,0 +1,15 @@
+#ifndef TAREA2_H
+#define TAREA2_H
+
+//Librerías
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <stdbool.h>
+
+//Funciones
+
+
+#endif // TAREA2_H
